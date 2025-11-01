fb5309cb4ebc9cce31467a163abdf60ecefba209
Thony Silva <thonysilva1993@gmail.com>
2025-11-01 01:08:33 -0500

    Actualizando

---
+++ Notes.md
@@ -0,0 +1,9 @@
+[GET]http://localhost:3001
+Hello World!
+[GET]http://localhost:3001/users
+[GET]http://localhost:3001/users/:id
+
+[POST]http://localhost:3001/users
+[PUT]http://localhost:3001/users/:id
+[DELETE]http://localhost:3001/users/:id
+
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Param } from '@nestjs/common';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  private readonly users: User[] = [
+    {
+      id: '1',
+      name: 'John Doe',
+      email: 'n3C6o@example.com',
+    },
+    {
+      id: '2',
+      name: 'Jane Smith',
+      email: 'LWb0G@example.com',
+    },
+  ];
+  @Get()
+  getAllUsers() {
+    return this.users;
+  }
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new HttpException('User with id ' + id + ' not found', HttpStatus.NOT_FOUND);
+    }
+    return user;
+  }
+}
