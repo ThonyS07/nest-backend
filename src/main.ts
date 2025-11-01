0ee21dfde132c876a51f5e604ed7fe89bb6e4a65
Thony Silva <thonysilva1993@gmail.com>
2025-11-01 00:42:49 -0500

    Actualizando cambios

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT ?? 3000;
+
+  await app.listen(port);
+  console.log(`🚀 Server running on port ${port}`);
 }
+
 bootstrap();
