3216391ea3238f433c38e40f8170acbbe72dd314
Thony Silva <thonysilva1993@gmail.com>
2025-11-02 17:53:50 -0500

    Add environment configuration and TypeORM setup for PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ENV } from './commons/interfaces/env.interface';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService<ENV>) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST', { infer: true }),
+        port: configService.get('POSTGRES_PORT', { infer: true }),
+        username: configService.get('POSTGRES_USERNAME', { infer: true }),
+        password: configService.get('POSTGRES_PASSWORD', { infer: true }),
+        database: configService.get('POSTGRES_DB', { infer: true }),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/commons/interfaces/env.interface.ts
+++ src/commons/interfaces/env.interface.ts
@@ -1,3 +1,9 @@
 export interface ENV {
   PORT: string;
+  DB_TYPE: 'postgres' | 'mysql' | 'sqlite' | 'mariadb' | 'mongodb';
+  POSTGRES_HOST: string;
+  POSTGRES_PORT: number;
+  POSTGRES_DB: string;
+  POSTGRES_USERNAME: string;
+  POSTGRES_PASSWORD: string;
 }
