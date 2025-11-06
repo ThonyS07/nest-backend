beb42d7aa2afe1862ed846a5687b098efede5e02
Thony Silva <thonysilva1993@gmail.com>
2025-11-05 20:32:53 -0500

    Add JWT and Passport JWT dependencies; enhance AuthController with login route and local authentication guard.

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import * as express from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  login(@Req() req: express.Request) {
+    return req.user;
+  }
 }
